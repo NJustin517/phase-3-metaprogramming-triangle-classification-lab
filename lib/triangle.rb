13e5534abac6e5f2d9f0ba6e538e7c3ff9276873
Justin Luciano <justinlluciano@gmail.com>
2022-02-03 08:43:38 -0800

    commit tree b599737f336a9a11d6bcbfced0893f7e217d0e40

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def side_equality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def validate_triangle
+    raise TriangleError unless side_equality? && sides_greater_than_zero?
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# Triangle.new(0, 0, 0).kind
